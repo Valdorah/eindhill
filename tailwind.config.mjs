49757a87b377b91c0aa03b7d6eb2b19d73efbcbd
Valdorah <alegoxis@proton.me>
2024-05-25 12:24:53 +0200

    :lipstick: Install shadcn.ui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,34 +1,72 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontFamily: {
-			sans: ['Sen', 'sans-serif']
-		},
-		extend: {
-			colors: {
-				'eindhill-1': '#261b15',
-				'eindhill-2': '#af9062',
-				'eindhill-3': '#c1ae9a',
-				'eindhill-4': '#623513',
-				'eindhill-5': '#e8dbbd',
-				'eindhill-6': '#d19f4f',
-				'eindhill-7': '#a76c26',
-				'eindhill-8': '#8c7660',
-				'eindhill-9': '#8a4e17',
-				'eindhill-10': '#886840',
-				'eindhill-11': '#edc96e',
-				'eindhill-12': '#ecbf50',
-				'eindhill-13': '#f7f3eb',
-				'eindhill-14': '#edd28b',
-				'eindhill-15': '#625342',
-				'eindhill-16': '#481205',
-				'eindhill-17': '#3c5444',
-				'eindhill-18': '#c46c24',
-				'eindhill-19': '#b4bc9c',
-				'eindhill-20': '#3c4c54'
-			}
-		},
-	},
-	plugins: [],
+module.exports = {
+  darkMode: ["class"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
 }
