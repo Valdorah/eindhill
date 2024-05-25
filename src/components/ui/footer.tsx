88b6636e3d5fbf3d0bb954efcdadd5d285247edc
Valdorah <alegoxis@proton.me>
2024-05-25 15:22:52 +0200

    :art: Add FontAwesome icons

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -4,13 +4,33 @@ import * as React from "react"
 
 export const Footer = () => {
     return (
-        <footer className="bg-secondary text-white p-4 flex justify-between items-center">
+        <footer
+            className="
+                bg-secondary
+                text-white
+                p-4
+                flex
+                justify-between
+                items-center
+            "
+        >
             <img
                 className="size-16 lg:size-32"
                 src="/images/eindhill_logo.png"
                 alt="eindhill_logo"
             />
-            <p>Discord</p>
+            <a
+                href="https://discord.gg/ME3Hbfty"
+                className="
+                    flex
+                    gap-4
+                    mr-3
+                    text-2xl
+                    md:text-4xl"
+            >
+                <i className="fa-brands fa-discord"></i>
+                <i className="fa-brands fa-instagram"></i>
+            </a>
         </footer>
     )
 }
